a9e4005e83e74bf841ace9a877a470326f29760b
aioria86 <juanmanuelccs19@gmail.com>
2019-07-22 20:02:17 -0500

    Commit creacion de tablas iniciales sin FK

---
+++ database/migrations/2019_07_23_005544_crear_tabla_usuario_rol.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaUsuarioRol extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario_rol', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('rol_id');
+            $table->unsignedInteger('usuario_id');
+            $table->boolean('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario_rol');
+    }
+}
